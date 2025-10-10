ad0c97f080a2ac4a0ab3850c80820ebc581335dc
harshadbk <khataleharshad26@gmail.com>
2025-10-10 22:09:42 +0530

    Create moveZeroes.harshadbk.cpp
    We’re not counting zeroes or creating a new array — just keeping track of where the next non-zero element should go.

---
+++ moveZeroes.harshadbk.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+    int j = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            swap(nums[i], nums[j]);
+            j++;
+        }
+    }
+}
+
+int main() {
+    vector<int> nums = {0, 1, 0, 3, 12};
+    
+    moveZeroes(nums);
+
+    cout << "Array after moving zeroes: ";
+    for (int num : nums) cout << num << " ";
+    cout << endl;
+
+    return 0;
+}

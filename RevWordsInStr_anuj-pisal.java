b5fe48c278eac27cc9110c023e3da1dfc8f66994
Anuj Pisal <157889578+anuj-pisal@users.noreply.github.com>
2025-10-11 20:44:44 +0530

    Create RevWordsInStr_anuj-pisal.java

---
+++ RevWordsInStr_anuj-pisal.java
@@ -0,0 +1,16 @@
+public class ReverseWords {
+    public static String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i != 0) sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "  the sky   is blue  ";
+        System.out.println(reverseWords(s));
+    }
+}
